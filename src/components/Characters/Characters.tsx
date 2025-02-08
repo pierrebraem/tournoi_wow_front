e1501f8b0af9aac9c6457209f435483380bfeea4
Pierre Braem <pierrebraem@orange.fr>
2025-02-08 13:25:43 +0100

    feat: Implémentation de la suppression d'un personnage

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -3,17 +3,47 @@ import AddDialog from "./AddDialog";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
 import { Button } from "primereact/button";
+import { ConfirmDialog } from "primereact/confirmdialog";
+import { confirmDialog } from "primereact/confirmdialog";
+import 'primeicons/primeicons.css';
 
 function Characters(){
     const [characters, setCharacters] = useState([]);
     const [visibleAdd, setVisibleAdd] = useState(false);
 
-    function dataFromAddDialog(visible){
+    function loadData(){
         fetch("http://localhost:3000/characters")
         .then(response => response.json())
         .then(data => setCharacters(data))
+    }
 
+    function dataFromAddDialog(visible){
+        loadData();
         setVisibleAdd(visible);
+    }
+
+    function confirmDelete(id, name){
+        confirmDialog({
+            message: "Etes-vous sur de supprimer le personnage" + name,
+            header: "Suppression",
+            accept: async () => {
+                await fetch("http://localhost:3000/characters/" + id, {
+                    method: "delete"
+                });
+
+                loadData();
+            }
+        });
+    }
+
+    function bodyIcons(rowData){
+        return(
+            <>
+                <Button icon="pi pi-book" />
+                <Button icon="pi pi-pencil" severity="warning" />
+                <Button icon="pi pi-trash" severity="danger" onClick={() => confirmDelete(rowData.id, rowData.name)}/>
+            </>
+        )
     }
 
     useEffect(() => {
@@ -30,10 +60,12 @@ function Characters(){
                 <Column field="role_id" header="Rôle"></Column>
                 <Column field="ilvl" header="ilvl"></Column>
                 <Column field="rio" header="rio"></Column>
+                <Column header="Action" body={bodyIcons}></Column>
             </DataTable>
 
             <Button label="Ajouter un personnage" onClick={() => setVisibleAdd(true)}/>
-            <AddDialog visible={visibleAdd} sendDataToParent={dataFromAddDialog}/>
+            <AddDialog visible={visibleAdd} sendDataToParent={dataFromAddDialog} />
+            <ConfirmDialog />
         </>
     )
 }
