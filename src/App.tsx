88aa69146fea9834bfbca9aa3cb8c931dfaf409b
Pierre Braem <pierrebraem@orange.fr>
2025-02-10 19:47:49 +0100

    refacto: déplacement header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { PrimeReactProvider } from 'primereact/api';
-import Header from './components/Home/Header/Header';
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Parties from './components/Parties/Parties';
 import Characters from './components/Characters/Characters';
