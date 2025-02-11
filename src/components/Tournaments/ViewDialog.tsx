9a7082c5715d670e432daf107158c0837b0f3126
Pierre Braem <pierrebraem@orange.fr>
2025-02-11 12:37:02 +0100

    feat: Implémentation termienr ou non donjon (en dev)

--- src/components/Tournaments/ViewDialog.tsx
+++ src/components/Tournaments/ViewDialog.tsx
@@ -5,6 +5,7 @@ import { Button } from "primereact/button";
 
 function ViewDialog({ visible, sendDataToParent, id, partiesOption }){
     const [dataParties, setDataParties] = useState([]);
+    const [dataChallenges, setDataChallenges] = useState([]);
     const [party, setParty] = useState({});
 
     function closeModal(){
@@ -15,6 +16,10 @@ function ViewDialog({ visible, sendDataToParent, id, partiesOption }){
         fetch("http://localhost:3000/registered/" + id)
         .then(response => response.json())
         .then(data => setDataParties(data))
+
+        fetch("http://localhost:3000/challenge/" + id)
+        .then(response => response.json())
+        .then(data => setDataChallenges(data))
     }
 
     async function addParty(){
@@ -24,6 +29,21 @@ function ViewDialog({ visible, sendDataToParent, id, partiesOption }){
         };
 
         await fetch("http://localhost:3000/registered/" + id, {
+            method: "put",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(body)
+        });
+
+        getData();
+    }
+
+    async function done(challenge_id, dungeos_id, party_id){
+        const body = {
+            challenge_id: challenge_id,
+            dungeos_id: dungeos_id,
+            party_id: party_id
+        }
+        await fetch("http://localhost:3000/registered/done", {
             method: "put",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(body)
@@ -46,6 +66,15 @@ function ViewDialog({ visible, sendDataToParent, id, partiesOption }){
                     <label>Ajouter une équipe</label>
                     <Dropdown value={party} onChange={(e) => setParty(e.value)} options={partiesOption} optionLabel="party_name" />
                     <Button label="Ajouter équipe" onClick={addParty}/>
+                </div>
+
+                <div>
+                    <p>Réalisation d'un donjon</p>
+                    <ul>
+                        {dataChallenges.map(challenge => (
+                            <li>{challenge.name} {challenge.party_name} {challenge.done ? "Fait ": "Non fait "} {challenge.done ? <Button label="Non terminé" onClick={() => undone(challenge.challenge_id, challenge.dungeos_id, challenge.party_id)}/>: <Button label="Terminé" onClick={() => done(challenge.challenge_id, challenge.dungeos_id, challenge.party_id)} />}</li>
+                        ))}
+                    </ul>
                 </div>
             </Dialog>
         </>
