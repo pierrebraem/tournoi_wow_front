ce48245e5dbe92a5c7e00d77923c319bc3d8e388
Pierre Braem <pierrebraem@orange.fr>
2026-08-15 15:08:54 +0200

    Erreurs

--- src/components/Characters/Characters.tsx
+++ src/components/Characters/Characters.tsx
@@ -18,6 +18,7 @@ function Characters(){
     const [refreshTrigger, setRefreshTrigger] = useState<number>(0);
 
     function dataFromDialog(){
+        console.log("coucou");
         setRefreshTrigger(prev => prev + 1);
         setVisibleDialog(false);
         setVisibleDetail(false);
